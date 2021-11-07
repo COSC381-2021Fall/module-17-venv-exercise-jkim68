c91e74e7db668e89150807971e28e911746189c4
Jacob Kim <jkim57@emich.edu>
2021-11-07 23:48:19 +0000

    add project

---
+++ import.py
@@ -0,0 +1,7 @@
+import pyjokes
+import emojis
+from oneliners import get_random
+j = get_random()
+print(j)
+emojis.encode('This is a message with emojis :smile: :snake:')
+print(pyjokes.get_joke())
